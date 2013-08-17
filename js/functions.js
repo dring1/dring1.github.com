d5502dbc8880c6e3cdbdedd589f1bbe85787257e
dring1 <devon.ring@gmail.com>
2013-08-17 10:51:25 -0400

    final stylization

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+function activate(){
+	btn = document.getElementById("email-icon");
+}
+
+function overlay() {
+	el = document.getElementById("overlay");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
